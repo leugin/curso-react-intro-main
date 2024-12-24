2ca7ef180dda7025075af29da58895cb485f0bf5
miguel <meqh1992@gmail.com>
2024-12-24 13:51:49 -0500

    feat: add save localstorage

--- src/App.js
+++ src/App.js
@@ -4,26 +4,48 @@ import { CreateTodoButton } from './CreateTodoButton'
 import { TodoCounter } from './TodoCounter';
 import { TodoItem } from './TodoItem';
 import React from 'react'
+const STORAGE_KEY  = 'TODOS_V1'
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
 
+// localStorage.setItem(STORAGE_KEY, JSON.stringify(defaultTodos));
+//localStorage.removeItem(STORAGE_KEY);
 
-const defaultTodos = [
-  {
-    text: "Cortar cebolla", completed: false
-  },
-  {
-    text: 'Tomar el curso de plazi ',  completed: true
-  },
-  {
-    text: 'lalala', completed: false
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+
+  let parsedItem;
+  
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
   }
-]
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
 function App() {
+  const [todos, saveTodos] = useLocalStorage(STORAGE_KEY, []);
+
   const [searchValue, SetSearchValue] = React.useState('');
-  const [todosValue, SetTodosValue] = React.useState(defaultTodos);
-  const total = todosValue.length
-  const progress = todosValue.filter(val => !!val.completed).length
- 
-  const todosSearched = todosValue
+  const total = todos.length
+  const progress = todos.filter(val => !!val.completed).length
+  
+  const todosSearched = todos
   .filter(val =>  val.
     text.toLowerCase()
     .includes(searchValue.toLowerCase()))
@@ -43,14 +65,14 @@ function App() {
           text={todo.text} 
           completed={todo.completed}
           onChange={(e)=> {
-            const newStat = [...todosValue]
+            const newStat = [...todos]
             newStat[index].completed= e.target.checked
-            SetTodosValue(newStat)
+            saveTodos(newStat)
           }}
           onDelete={()=>{
-            const newStat = [...todosValue]
+            const newStat = [...todos]
             newStat.splice(index, 1)
-            SetTodosValue(newStat)
+            saveTodos(newStat)
           }}
           />
         ))}
